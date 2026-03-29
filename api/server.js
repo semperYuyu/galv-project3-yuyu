49d61426a75e051f2218f21d7280a8a139b2841d
Tafari <tafarigriffith2016@gmail.com>
2026-03-29 17:18:26 -0600

    Starts implementation of db into server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const server = express();
 const PORT = 8080;
+const knex = require("knex")(require("./knexfile.js")["development"]);
 
-server.get("/", (req, res) => {
-  res.status(200).send(":D :3");
+server.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
+});
+
+server.get("/:table", (req, res) => {
+  let { table } = req.params;
+  knex(table)
+    .select("*")
+    .then((data) => res.status(200).send(data))
+    .catch(() => res.status(200).send("sowwy that doesnt exist"));
 });
 
 server.listen(PORT, () => {
